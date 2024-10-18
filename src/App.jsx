7f9ded132c6772c4a86c016cdd7662b2ddda2bc4
IMRAN <Zeeshan@2024gmail.com>
2024-10-18 16:57:30 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import "./App.css";
 import { Search, MapPin, Wind } from "react-feather";
 import getWeather from "./api/api";
 import { useState } from "react";
 import dateFormat from "dateformat";
+import "./App.css"; 
 
 function App() {
   const [city, setCity] = useState("");
@@ -20,61 +20,63 @@ function App() {
   };
 
   return (
-    <div className="app">
-      <h1>Weather App</h1>
-      <div className="input-wrapper">
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Enter City Name"
-        />
-        <button onClick={() => getWeatherbyCity()}>
-          <Search></Search>
-        </button>
+    <div className="bg-black">
+      <div className="app">
+        <h1>Weather App</h1>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter City Name"
+          />
+          <button onClick={() => getWeatherbyCity()}>
+            <Search />
+          </button>
+        </div>
+
+        {weather && weather.weather && (
+          <div className="content">
+            <div className="location d-flex">
+              <MapPin />
+              <h2>
+                {weather.name} <span>({weather.sys.country})</span>
+              </h2>
+            </div>
+            <p className="datetext">{renderDate()}</p>
+
+            <div className="weatherdesc d-flex flex-c">
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt=""
+              />
+              <h3>{weather.weather[0].description}</h3>
+            </div>
+
+            <div className="tempstats d-flex flex-c">
+              <h1>
+                {weather.main.temp} <span>&deg;C</span>
+              </h1>
+              <h3>
+                Feels Like {weather.main.feels_like} <span>&deg;C</span>
+              </h3>
+            </div>
+
+            <div className="windstats d-flex">
+              <Wind />
+              <h3>
+                Wind is {weather.wind.speed} Knots in {weather.wind.deg}&deg;
+              </h3>
+            </div>
+          </div>
+        )}
+
+        {!weather.weather && (
+          <div className="content">
+            <h4>No Data found!</h4>
+          </div>
+        )}
       </div>
-
-      {weather && weather.weather && (
-        <div className="content">
-          <div className="location d-flex">
-            <MapPin></MapPin>
-            <h2>
-              {weather.name} <span>({weather.sys.country})</span>
-            </h2>
-          </div>
-          <p className="datetext">{renderDate()}</p>
-
-          <div className="weatherdesc d-flex flex-c">
-            <img
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt=""
-            />
-            <h3>{weather.weather[0].description}</h3>
-          </div>
-
-          <div className="tempstats d-flex flex-c">
-            <h1>
-              {weather.main.temp} <span>&deg;C</span>
-            </h1>
-            <h3>
-              Feels Like {weather.main.feels_like} <span>&deg;C</span>
-            </h3>
-          </div>
-
-          <div className="windstats d-flex">
-            <Wind></Wind>
-            <h3>
-              Wind is {weather.wind.speed} Knots in {weather.wind.deg}&deg;
-            </h3>
-          </div>
-        </div>
-      )}
-
-      {!weather.weather && (
-        <div className="content">
-          <h4>No Data found !</h4>
-        </div>
-      )}
     </div>
   );
 }
